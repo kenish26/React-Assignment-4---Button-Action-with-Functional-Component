3ac4edd1fc89037d75ee11687af677d6772d01e3
kenish <ahujakenish123@gmail.com>
2021-02-22 04:31:50 +0530

    "First"

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
-import React, {Component, useState} from "react";
+import React, {Component, useEffect, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [paragraph,setParagraph]=useState(<p id="para"></p>);
+  
+ const handleClick=()=>
+ {
+   setParagraph(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"</p>);
+   console.log(paragraph);
+ }
+
+
   return (
     <div id="main">
-      // Do not alter the main div
+      {paragraph}
+      <button 
+      id="click"
+      onClick={handleClick}>
+        Click me
+      </button>
+      
     </div>
   );
 }
