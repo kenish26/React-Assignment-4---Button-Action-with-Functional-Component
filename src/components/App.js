21dacf70ff77c9415f52ba9ce834636170930a44
kenish <ahujakenish123@gmail.com>
2021-02-22 05:40:46 +0530

    j

--- src/components/App.js
+++ src/components/App.js
@@ -13,17 +13,18 @@ function App() {
 
   return (
     <div id="main">
-      <button 
-      id="click"
-      onClick={handleClick}>
-        Click me
-      </button>
+     
 
       {cond ? 
             <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
             :
             <div></div>
       }
+            <button 
+      id="click"
+      onClick={handleClick}>
+        Click me
+      </button>
       
       
     </div>
